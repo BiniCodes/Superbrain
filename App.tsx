15ae710d68ddf3930f9520738101cb94b1eac2dc
Viet Duc Kieu <viet.duc.kieu@web.de>
2019-07-23 10:51:38 +0200

    change everything

--- App.tsx
+++ App.tsx
@@ -1,26 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
-import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-
-export default function App() {
-  let pic = {
-    uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-  } 
-
-  return (
-    <View style={styles.container}>
-      <Text>Hello, world!</Text>
-      <Text>My name is Duc</Text>
-      <TextInput></TextInput>
-    </View>
-  );
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Hello, world!</Text>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
